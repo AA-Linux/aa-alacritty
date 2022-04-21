a3fb067899d2a32efcef508265a55e29d45991f3
AlphaLawless <alpha.vylly12@ufpi.edu.br>
2022-04-21 18:36:34 -0300

    up: ⚪ initial files 🔴

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ install_alacritty() {
         cp -r `pwd`/files/*  "$HOME"/.config/alacritty
         echo -e "\n\e[0;32m+-----------Finished!-----------+\e[0m"
     else
-        echo -e "\e[0;32m+---Install Alacritty---+\e[0m"
+        echo -e "\e[0;32m+---Install Alacritty---+\e[0m\n"
         sudo pacman -S alacritty
         echo -e "\e[0;32m Copy the files for your .config\e[0m"
         cp -r `pwd`/files/*  "$HOME"/.config/alacritty
